7a824029b037952b8294940947c4e3b99d90f534
Thelma Boamah <thelma.a.boamah@gmail.com>
2023-08-29 17:31:15 -0400

    working basic modal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+// import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [open, setOpen] = useState(false);
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const { current: el } = modalRef;
+    if (open) {
+      el.showModal();
+    } else {
+      el.close();
+    }
+  }, [open]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <button onClick={() => setOpen(!open)}>Test button</button>
+      <dialog ref={modalRef}>
+        <button onClick={() => setOpen(false)}>Close</button>
+        <div>test</div>
+      </dialog>
+    </div>
+  );
 }
 
-export default App
+export default App;
