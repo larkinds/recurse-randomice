6da9510fea4c363bc44e172725e61f3a5ce49f2d
Manuel Sabin <msabin27@gmail.com>
2023-08-29 15:28:43 -0700

    Made Quantity component, Item component, made array of items and can display. :)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,13 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 // import './App.css';
 
+const itemList = [
+  {flavor: "strawberry", price: 0, quantity: 1},
+  {flavor: "chocolate", price: 0, quantity: 3},
+  {flavor: "vanilla", price: 0, quantity: 2}
+]
+
+
 function App() {
   const [open, setOpen] = useState(false);
   const modalRef = useRef(null);
@@ -18,13 +25,44 @@ function App() {
 
   return (
     <div>
-      <button onClick={() => setOpen(!open)}>Test button</button>
+      <button onClick={() => setOpen(!open)}>Cart</button>
       <dialog ref={modalRef}>
         <button onClick={() => setOpen(false)}>Close</button>
+        
+        {itemList.map((product) => <div> <Item item={product} /> </div>)}
+        
+
         <div>test</div>
       </dialog>
     </div>
   );
+}
+
+
+function Item({ item }){
+  return(
+    <div>
+      <img />
+      <p> {item.flavor} </p>
+      <button> Remove </button>
+
+
+      <p> ${item.price} </p>
+      <Quantity quantity={item.quantity}/>
+    </div>
+  )
+}
+
+function Quantity({ quantity }){
+  const [count, setCount] = useState(quantity)
+
+  return(
+    <>
+      <button onClick={() => setCount((c) => c-1)}> - </button>
+      <span> {count} </span>
+      <button onClick={() => setCount((c) => c+1)}> + </button>
+    </>
+  )
 }
 
 export default App;
