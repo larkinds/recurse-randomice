0ed70cf3500e250f4ffb88af558db19bd42cb2e3
Thelma Boamah <thelma.a.boamah@gmail.com>
2023-09-05 14:47:14 -0400

    refactor and add type for button

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,3 +1,11 @@
-export default function Button({ action, children }) {
-  return <button onClick={action}>{children}</button>;
+type ButtonProps<T> = {
+  action: (args?: T) => void;
+  children: React.ReactNode;
+};
+
+export default function Button<T>({ action, children }: ButtonProps<T>) {
+  function handleClick() {
+    action();
+  }
+  return <button onClick={handleClick}>{children}</button>;
 }
