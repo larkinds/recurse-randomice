f32dc0775470fcf790724e6fbbd7d1d74e0ebe98
Josh Geselowitz <joshgeselowitz@Joshs-MBP.lan>
2023-09-05 13:58:18 -0400

    order item v2

--- client/src/components/OrderItem.tsx
+++ client/src/components/OrderItem.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './OrderItem.css'
+import React from "react";
+import "./OrderItem.css";
 
 interface OrderProps {
   price: number;
@@ -8,13 +8,17 @@ interface OrderProps {
   flavor: string;
 }
 
-function OrderItem({price, img, quantity, flavor} : OrderProps) {
+function OrderItem({ price, img, quantity, flavor }: OrderProps) {
   return (
     <div className="order-item-container">
-      <div>
-        <img className="order-image" alt="ice cream cone" src={img}></img>
+      <div className="image-container">
+        <img
+          className="order-image"
+          alt="ice cream cone"
+          src={"../public/ice_cream.jpeg"}
+        ></img>
       </div>
-      <div className="item-details">
+      <div className="details-container">
         <div className="item-info">I am the info</div>
         <span>{price}</span>
         <div className="price-total"> price and trash can</div>
