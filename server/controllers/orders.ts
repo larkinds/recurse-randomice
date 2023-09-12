aa3bd9f7f568bc2fdd4e49e8c882559aa5887628
Josh Geselowitz <joshgeselowitz@Joshs-MacBook-Pro.local>
2023-09-12 16:14:59 -0400

    Orders table added

--- server/controllers/orders.ts
+++ server/controllers/orders.ts
@@ -7,11 +7,8 @@ orderRouter.use(express.json())
 
 
 orderRouter.post('/', async(req: Request, res: Response, next: NextFunction) => {
-    try{
-        console.log(req.body)
-        const body = req.params
-      
-        if (!body.userID) {
+    try{      
+        if (!req.body.userID) {
           res.status(400).json({ 
             error: 'content missing' 
           }).send()
@@ -21,12 +18,49 @@ orderRouter.post('/', async(req: Request, res: Response, next: NextFunction) => 
       
         const insertedOrder = await order.save();
         res.status(201).json(insertedOrder).send();
-    
+        return;
         } catch (error) {
             console.log(error)
             res.status(400).json({error}).send("message")
         }
-      }) 
+      })
+
+orderRouter.get('/userID/:userID', async(req: Request, res: Response) => {
+        try{
+          const userID = req.params.userID
+          const order = await Orders.findOne({userID: userID}) 
+          res.status(200).json(order).send()
+        }
+        catch (error) {
+          console.log(error)
+          res.status(400).json({error}).send("message")
+        }
+      })
+   
+orderRouter.get('/id/:id', async(req: Request, res: Response) => {
+        try{
+          const orderID = req.params.id
+          const order = await Orders.findById(orderID) 
+          res.status(200).json(order).send()
+        }
+        catch (error) {
+          console.log(error)
+          res.status(400).json({error}).send("message")
+        }
+      
+      })
+
+orderRouter.get('/', async(req: Request, res: Response) => {
+  try{
+    const orders = await Orders.find({}) 
+    res.status(200).json(orders).send()
+  }
+  catch (error) {
+    console.log(error)
+    res.status(400).json({error}).send("message")
+  }
+
+})
 
 
 
