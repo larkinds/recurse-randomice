c0764ecc0315cb4fb249339a536433980612d637
pearlescence-m <meruyert.jakipbekova@gmail.com>
2023-09-13 00:01:16 -0400

    formatted with prettier and changed to Orders in schema

--- server/models/OrderItem.ts
+++ server/models/OrderItem.ts
@@ -2,28 +2,28 @@ import mongoose from 'mongoose'
 import uniqueValidator from 'mongoose-unique-validator'
 
 export interface OrderItems extends mongoose.Document {
-    quantity: number
-    orderId: string
-    iceCreamId: string
+  quantity: number
+  orderId: string
+  iceCreamId: string
 }
 
 const OrderItemSchema = new mongoose.Schema<OrderItems>({
-    quantity: {
-        type: Number,
-        required: true,
-    },
-    orderId: {
-        type: String || mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'Order'
-    },
-    iceCreamId: {
-        type: String || mongoose.Schema.Types.ObjectId,
-        ref: 'Icecream',
-    },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  orderId: {
+    type: String || mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Orders',
+  },
+  iceCreamId: {
+    type: String || mongoose.Schema.Types.ObjectId,
+    ref: 'Icecream',
+  },
 })
 
 OrderItemSchema.plugin(uniqueValidator)
 
 export default mongoose.models.OrderItem ||
-    mongoose.model<OrderItems>('OrderItem', OrderItemSchema)
+  mongoose.model<OrderItems>('OrderItem', OrderItemSchema)
