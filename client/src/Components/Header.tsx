2208ecb845018365e82a81bc32230f743e838c9c
Lee Sager <dleesager1@gmail.com>
2023-09-19 13:57:11 -0400

    implement show/hide nav page function and styles

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import IceCreamLogo from "./Logo";
 import styles from "./header.module.css";
 
 export default function IceCreamHeader() {
+  const [page, setPage] = useState("/");
+  let currentPage = page;
+
+  function showNavOption(page: string) {
+    return page == currentPage;
+  }
+
   return (
     <header className={styles["header-container"]}>
       {/* icon - site name (links to home) - nav bar - login/out - cart icon/link */}
@@ -11,17 +19,23 @@ export default function IceCreamHeader() {
       </a>
       <nav>
         <ul className={styles["nav-container"]}>
-          <li className={styles["nav-item"]}>
+          <li
+            className={styles["nav-item"]}
+            hidden={showNavOption("/hall-of-fame")}
+          >
             {/* make these routes actually route */}
             <a href="/hall-of-fame">Hall of Fame</a>
           </li>
-          <li className={styles["nav-item"]}>
+          <li
+            className={styles["nav-item"]}
+            hidden={showNavOption("/random-flavors")}
+          >
             <a href="/random-flavors">Generate Random Flavors</a>
           </li>
         </ul>
       </nav>
       <div>
-        <button>login/logout</button>
+        <button className={styles["login-button"]}>login/logout</button>
         <a href="/cart">cart logo</a>
       </div>
     </header>
