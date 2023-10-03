2af5c6035ba6798729f0a536e7a1b0246be22914
Larkin <larkindsmith@gmail.com>
2023-10-03 12:26:05 -0400

    Updated useLocalStorage to set based on category

--- client/src/hooks/UseLocalStorage.tsx
+++ client/src/hooks/UseLocalStorage.tsx
@@ -1,20 +1,32 @@
 import { useState, useEffect } from "react";
 import { StorageData } from "../utils/Types";
 
-//to import these functions, destructure the function that you'd like to use just like a built in hook:
-// const { setItemInLocalStorage, fetchItemFromLocalStorage, fetchAllItemsFromLocalStorage } = useSetLocalStorage()
+//to import these functions, destructure the function that you'd like to use just like a built in hook. This hook should only be called at the top level, and then the destructured parts passed down.
+//  const [value, setValue] = useSetLocalStorage(localStorageCopy, "Test");
 
 export default function useSetLocalStorage(storageItem: StorageData, fallbackState: string) {
     const user = localStorage.getItem("user");
-    const cart = localStorage.getItem("cart")
-    //danger: what if both user and cart are there?
+    const cart = localStorage.getItem("cart");
     
-    const [value, setValue] = useState<StorageData>(storageItem ?? {user, cart} ?? fallbackState);
+    const [value, setValue] = useState(storageItem ?? {user, cart} ?? fallbackState);
 
     useEffect(() => {
-        console.log("useEffect")
-        if (value !== fallbackState) {
-            localStorage.setItem(value.key, JSON.stringify(value.value));
+        let key: string;
+        if (value.cart && value.user) {
+            key = "both";
+        } else if (value.cart) {
+            key = "cart"
+        } else {
+            key = "user"
+        }
+
+        if (value !== fallbackState && key === "both") {
+            localStorage.setItem("cart", JSON.stringify(value.cart));
+            localStorage.setItem("user", JSON.stringify(value.user));
+        } else if (value !== fallbackState && key === "user") {
+            localStorage.setItem("user", JSON.stringify(value.user));
+        } else if (value !== fallbackState && key === "cart") {
+            localStorage.setItem("cart", JSON.stringify(value.cart));
         }
     }, [value, setValue, fallbackState])
 
