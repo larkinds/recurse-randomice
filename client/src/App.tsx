b4e3f69ceec3dfd0fb150d1f10659ad64ed76e71
Larkin <larkindsmith@gmail.com>
2023-10-04 14:04:58 -0400

    Updated App.tsx to include the context provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,48 +1,13 @@
 import useSetLocalStorage from "./hooks/UseLocalStorage";
 import { LocalStorageContext } from "./context/DataContext";
-import { StorageData } from "./utils/Types";
-import ProductCard from "./components/ProductCard";
 
-
-const fakeUser: StorageData = {
-  user: "Larkin",
-  cart: {
-    iceCream: [{
-      name: "relucantant lemon",
-      quantity: 3
-    }],
-    toppings: [{
-      name: "skittles",
-      quantity: 1
-    }]
-  }
-};
-
-const fakeUserTwo: StorageData = {
-  user: "Josh",
-  cart: {
-    iceCream: [{
-      name: "strawberry shortcake",
-      quantity: 5
-    }],
-    toppings: [{
-      name: "sprinkles",
-      quantity: 1
-    }]
-  }
-};
 
 export default function App() {
   const [storage, setStorage] = useSetLocalStorage(null, "Test");
 
   return (
     <LocalStorageContext.Provider value={storage}>
-      <div>
-        <button onClick={() => setStorage(fakeUser)}>1</button>
-        <button onClick={() => setStorage(fakeUserTwo)}>2</button>
-        <p>{storage?.user}</p>
-      </div>
-      <ProductCard name={""} description={""} image={""} creator={""} purchaseHistory={0} />
+      <div></div>
     </LocalStorageContext.Provider>
   );
 }
