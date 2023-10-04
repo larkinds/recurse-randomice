a691963d4dbb01b3e0d2fdb0e45d98163e0bf596
Josh Geselowitz <joshgeselowitz@Joshs-MacBook-Pro.local>
2023-10-04 16:44:10 -0400

    temp single page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,24 +2,26 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import HallOfFame from "./pages/hallOfFame/HallOfFame";
 import ErrorPage from "./pages/error/Error";
+import FlavorPage from "./pages/flavor_page/FlavorPage";
 import useSetLocalStorage from "./hooks/UseLocalStorage";
 import { LocalStorageContext } from "./context/DataContext";
 
 //todo: replace temp with homepage
 export default function App() {
   const [storage, setStorage] = useSetLocalStorage(null, "Test");
- 
+
   return (
     <LocalStorageContext.Provider value={storage}>
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<div>Temp</div>}  />
-          <Route path="hall-of-fame" element={<HallOfFame/>}/>
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<div>Temp</div>} />
+            <Route path="hall-of-fame" element={<HallOfFame />} />
+            <Route path="flavor/*" element={<FlavorPage />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
     </LocalStorageContext.Provider>
-  )
+  );
 }
---
+++ client/src/pages/flavor_page/FlavorPage.tsx
@@ -0,0 +1,13 @@
+import styles from ".flavorpage.module.css";
+import React from "react";
+import IceCreamHeader from "../../components/Header";
+
+function FlavorPage() {
+  return (
+    <>
+      <IceCreamHeader></IceCreamHeader>
+    </>
+  );
+}
+
+export default FlavorPage;
