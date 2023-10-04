cff29df6494e7806f54fe092302a4dee23d04981
pearlescence-m <meruyert.jakipbekova@gmail.com>
2023-10-04 17:33:03 -0400

    initialized and added tailwind

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import FlavorThumbnail, { Flavor } from "../../components/FlavorThumbnail";
 import iceCreamService from "../../services/icecreams";
-import styles from "./home.module.css";
+// import styles from "./home.module.css";
 
 const makePhrases = async (len: number) => {
   const userFlavors: Flavor[] = new Array<Flavor>();
@@ -35,30 +35,41 @@ export default function HomePage() {
   }, []);
 
   async function handleRegenerateClick() {
-    console.log("clicked");
     setNewFlavor((await makePhrases(1)).pop());
   }
 
   return (
     <>
-
+    <div className="relative isolate px-6 pt-14 lg:px-8">
+    <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+      <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" ></div>
+    </div>
+    </div>
+    <div className="mx-auto max-w-2xl lg:text-center">
+      <h2 className="text-base font-semibold leading-7 text-indigo-600">Ice cream store</h2>
+      <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Randomice</h1>
+      <p className="mt-6 text-lg leading-8 text-gray-600">Enjoy the taste of our randomly generated silly flavors!</p>
+    </div>
       {newFlavor && (
         <div>
           <button onClick={handleRegenerateClick}>Regenerate</button>
           <FlavorThumbnail flavor={newFlavor} />
         </div>
       )}
-      <div className={styles.container}>
+      <div className="flex justify-center flex-wrap max-w-xl gap-x-5 gap-y-10 lg:max-w-none lg:gap-x-10 lg:gap-y-50 lg:m-10">
         {generatedFlavors.map((flavor) => (
           <FlavorThumbnail key={flavor.name} flavor={flavor} />
         ))}
       </div>
 
-      <div className={styles.container}>
+      <div className="flex justify-center flex-wrap max-w-xl gap-x-5 gap-y-10 lg:max-w-none lg:gap-x-10 lg:gap-y-50 lg:m-10">
         {userFlavors.map((flavor) => (
           <FlavorThumbnail key={flavor.name} flavor={flavor} />
         ))}
       </div>
+      <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]" aria-hidden="true">
+      <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(10%+36rem)] sm:w-[72.1875rem]" ></div>
+    </div>
     </>
   );
 }
---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
