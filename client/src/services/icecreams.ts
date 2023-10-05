e06448277d52a3914f49ca624142529089baeb66
pearlescence-m <meruyert.jakipbekova@gmail.com>
2023-10-04 20:34:34 -0400

    added error handling for home resvices

--- client/src/services/icecreams.ts
+++ client/src/services/icecreams.ts
@@ -3,18 +3,30 @@ const iceCreamUrl = "http://localhost:3003/api/icecreams/suggestions/5";
 const phraseUrl = "http://localhost:3003/api/words/";
 
 const getFive = () => {
-  const request = axios.get(iceCreamUrl);
-  return request.then((response) => response.data);
+  const request = axios.get(iceCreamUrl)
+  .then((response) => response.data)
+  .catch(function (error) {
+    console.log(error.toJSON());
+  });
+  return request
 };
 
 const getAdjective = async () => {
-  const request = axios.get(`${phraseUrl}/adjective`);
-  return request.then((response) => response.data);
+  const request = axios.get(`${phraseUrl}/adjective`)
+  .then((response) => response.data)
+  .catch(function (error) {
+    console.log(error.toJSON());
+  });
+  return request
 };
 
 const getNoun = async () => {
-  const request = axios.get(`${phraseUrl}/noun`);
-  return request.then((response) => response.data);
+  const request = axios.get(`${phraseUrl}/noun`)
+  .then((response) => response.data)
+  .catch(function (error) {
+    console.log(error.toJSON());
+  });
+  return request
 };
 
 export default { getFive, getAdjective, getNoun };
