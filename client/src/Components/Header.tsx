683d55fcda98402dacece26a13241177227fff11
eyeriss42 <iris.mck@pm.me>
2023-12-06 01:16:16 +0600

    add hover color

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -2,27 +2,30 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import IceCreamLogo from "./IceCreamLogo";
 import CartPage from "../pages/cart/CartPage";
-import styles from "./header.module.css";
-
 
 export default function IceCreamHeader() {
   const [currentPagePath, setCurrentPagePath] = useState<string>("/");
-  
+
   function handleSetStarPage(path: string) {
-    setCurrentPagePath(path)
+    setCurrentPagePath(path);
   }
 
   return (
-    <header className={styles["header-container"]}>
-      <Link to="/" onClick={() => handleSetStarPage("/")}><IceCreamLogo />Randomice</Link>
+    <header className="w-full flex justify-between items-center h-[100px] px-4">
+      <Link to="/" onClick={() => handleSetStarPage("/")}>
+        <IceCreamLogo />
+        Randomice
+      </Link>
       <nav>
-        <ul className={styles["nav-container"]}>
+        <ul className="flex justify-center list-none mb-0">
           <NavOption
+            className="flex"
             destinationUrl="/hall-of-fame"
             destinationPage="Hall of Fame"
             currentPagePath={currentPagePath}
             setCurrentPagePath={setCurrentPagePath}
           />
+
           <NavOption
             destinationUrl="/random-flavors"
             destinationPage="Generate Random Flavors"
@@ -31,8 +34,8 @@ export default function IceCreamHeader() {
           />
         </ul>
       </nav>
-      <div className={styles["header-end"]}>
-        <button className={styles["login-button"]}>login/logout</button>
+      <div className="flex min-w-[10rem]">
+        <button className="mr-2.5">login/logout</button>
         <CartPage />
       </div>
     </header>
@@ -40,17 +43,28 @@ export default function IceCreamHeader() {
 }
 
 interface NavOptionProps {
+  className?: string;
   destinationUrl: string;
   destinationPage: string;
   currentPagePath: string;
   setCurrentPagePath(pageUrl: string): void;
 }
 
-function NavOption(props: NavOptionProps) {
+function NavOption({ className, destinationUrl, destinationPage, currentPagePath, setCurrentPagePath }) {
+  const isActive = currentPagePath === destinationUrl;
+  const linkBaseClasses = "text-center block py-2 px-4 rounded w-auto whitespace-nowrap";
+  const activeClasses = "border bg-purple-500 hover:bg-purple-700 text-white";
+  const inactiveClasses = "border border-white text-black-500 hover:border-black hover:bg-white";
+
   return (
-    <li className={styles["nav-item"]}>
-      <Link to={props.destinationUrl} onClick={() => props.setCurrentPagePath(props.destinationUrl)}>{props.currentPagePath === props.destinationUrl ? "*" : ""} 
-        {props.destinationPage}</Link>
+    <li className={`${className}`}>
+      <Link
+        to={destinationUrl}
+        className={`${linkBaseClasses} ${isActive ? activeClasses : inactiveClasses}`}
+        onClick={() => setCurrentPagePath(destinationUrl)}
+      >
+        {destinationPage}
+      </Link>
     </li>
   );
 }
