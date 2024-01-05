d28660d161f8c903caa43c88ea4bc016179d4e1b
Larkin <larkindsmith@gmail.com>
2024-01-05 16:34:45 -0500

    Added key to reset components state

--- client/src/pages/singleProduct/SingleProduct.tsx
+++ client/src/pages/singleProduct/SingleProduct.tsx
@@ -25,6 +25,7 @@ export default function SingleProduct() {
   return (
     <div className={styles.container}>
       <ProductCard
+        key={state.flavor.name}
         name={state.flavor.name}
         image={state.flavor.imageURL}
         creator={state.flavor.userId}
